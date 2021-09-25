6236f5790f376504b3826a38fe38a664f73eb665
Dima Portenko <troublesohard@gmail.com>
2021-09-25 12:46:12 +0300

    feat: druid hello world app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use druid::{AppLauncher, WindowDesc, Widget, PlatformError};
+use druid::widget::Label;
+
+fn build_ui() -> impl Widget<()> {
+    Label::new("Hello world")
+}
+
+fn main() -> Result<(), PlatformError> {
+    let root = || {
+        build_ui()
+    };
+    AppLauncher::with_window(
+        WindowDesc::new(root)
+    ).launch(())?;
+    Ok(())
 }
