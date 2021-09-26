a06ef038da26ac67f013aafe784875b5757b16cc
Dima Portenko <troublesohard@gmail.com>
2021-09-27 00:44:37 +0300

    feat: add window size and title
    new file for app state

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
-use druid::{AppLauncher, WindowDesc, Widget, PlatformError};
-use druid::widget::{Label, Flex, Padding};
+use druid::{AppLauncher, WindowDesc, Widget, PlatformError, Color};
+use druid::widget::{Label, Flex, Padding, Container};
 
 fn build_ui() -> impl Widget<()> {
-    Padding::new(
-        10.0,
-        Flex::row()
-            .with_flex_child(
-                Flex::column()
-                    .with_flex_child(Label::new("top left"), 1.0)
-                    .with_flex_child(Label::new("bottom left"), 1.0),
-                1.0)
-            .with_flex_child(
-                Flex::column()
-                    .with_flex_child(Label::new("top right"), 1.0)
-                    .with_flex_child(Label::new("bottom right"), 1.0),
-                1.0),
-    )
+    Container::new(
+        Padding::new(
+            10.0,
+            Flex::row()
+                .with_flex_child(
+                    Flex::column()
+                        .with_flex_child(Label::new("top left"), 1.0)
+                        .with_flex_child(Label::new("bottom left"), 1.0),
+                    1.0)
+                .with_flex_child(
+                    Flex::column()
+                        .with_flex_child(Label::new("top right"), 1.0)
+                        .with_flex_child(Label::new("bottom right"), 1.0),
+                    1.0),
+        )
+    ).background(Color::BLACK)
 }
 
 fn main() -> Result<(), PlatformError> {
     let root = || {
         build_ui()
     };
+    let window = WindowDesc::new(root)
+        .window_size((720., 600.))
+        .with_min_size((620., 300.))
+        .title("Quick Paste");
     AppLauncher::with_window(
-        WindowDesc::new(root)
+        window
     ).launch(())?;
     Ok(())
 }
