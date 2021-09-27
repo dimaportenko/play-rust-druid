f00849954d1c6f7608d5b8ff493d4c5a25ca561e
Dima Portenko <troublesohard@gmail.com>
2021-09-27 23:54:46 +0300

    feat: switch to main branch druid dependecies
    set transparent window

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,16 @@ fn build_ui() -> impl Widget<()> {
                         .with_flex_child(Label::new("bottom right"), 1.0),
                     1.0),
         )
-    ).background(Color::BLACK)
+    )
+        // .background(Color::rgba(1., 1.5, 1.5, 0.9))
+        .background(Color::GRAY.with_alpha(0.9))
 }
 
 fn main() -> Result<(), PlatformError> {
-    let root = || {
-        build_ui()
-    };
-    let window = WindowDesc::new(root)
+    let window = WindowDesc::new(build_ui())
         .window_size((720., 600.))
         .with_min_size((620., 300.))
+        .transparent(true)
         .title("Quick Paste");
     AppLauncher::with_window(
         window
